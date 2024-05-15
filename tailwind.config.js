9ba854e9216ec4df56f05ba253b676109510414a
Somanath S Kakade <somanath.kakade5@gmail.com>
2024-05-15 11:54:26 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+
+      },
+      colors:{
+
+      },
+      container:{
+        center:true,
+        padding:{
+          DEFAULT:"1rem",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
